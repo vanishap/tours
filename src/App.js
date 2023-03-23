5c00c354a9a0b128af91e66e08b1726b9e14e343
vanishap <vanishareddyp@gmail.com>
2023-03-22 20:43:05 -0700

    delete tour

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import { useState, useEffect } from 'react';
 import Loading from './Loading';
 import Tours from './Tours';
 
+
 const url = 'https://course-api.com/react-tours-project';
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours]= useState([]);
+
+  const deleteTour = (id)=>{
+       const newTours=  tours.filter(tour=>tour.id !== id);
+       setTours(newTours);
+    }
 
   const getTourInfo = async()=>{
     setLoading(true);
@@ -27,9 +33,19 @@ function App() {
       </main>
     )
   }
+  if(tours.length === 0){
+    return (
+    <main>
+      <div className="title">
+        <h2>No tours left</h2>
+        <button className = 'btn' onClick={()=>getTourInfo()}>Refresh</button>
+      </div>
+    </main>
+    )
+  }
   return (
     <main >
-    <Tours tours ={tours}/> 
+    <Tours tours ={tours} deleteTour={deleteTour}/> 
     </main>
   );
 }
