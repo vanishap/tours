da9c2d928de1a5fc0b7540e6931def247bf6b456
vanishap <vanishareddyp@gmail.com>
2023-03-22 17:24:39 -0700

    added toggle to info

--- src/Tour.js
+++ src/Tour.js
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const Tour = (tour)=>{
+    const [readMore, setReadMore] = useState(false);
     return (
         <article className="single-tour" >
             <img src={tour.image} alt={tour.name} />
@@ -7,7 +10,9 @@ const Tour = (tour)=>{
                     <h4>{tour.name}</h4>
                     <h4 className="tour-price">${tour.price}</h4>
                 </div>
-                <p>{tour.info}</p>
+                <p>{readMore ? `${tour.info}`: `${tour.info.substring(0,250)}...`}
+                <button onClick={()=>{setReadMore(!readMore)}}>{readMore? 'Show Less' : 'Read More'}</button>
+                </p>
                 <button className="delete-btn">Delete</button>
             </footer>
         </article>
